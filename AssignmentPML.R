6a79f3f468a996cce14b0efc0c087595c1a71877
Claudio <10401917+claudiolande@users.noreply.github.com>
2019-10-18 16:36:23 +0200

    Delete AssignmentPML.R

--- AssignmentPML.R
+++
@@ -1,7 +0,0 @@
-trainFile = "./pml-training.csv"
-testFile  = "./pml-testing.csv"
-
-trainDS <- read.csv(trainFile, stringsAsFactors = FALSE)
-testDS  <- read.csv(trainFile, stringsAsFactors = FALSE)
-
-lm1 <- lm(classe ~ ., data=trainDS)
